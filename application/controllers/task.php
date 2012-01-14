3fb7811c72bd09bad7710cfe116fa5d48357c0f0
amitavroy <amitavroy@gmail.com>
2012-01-14 09:01:05 +0000

    Adding to the online code hosting.

---
+++ application/controllers/task.php
@@ -0,0 +1,40 @@
+<?php
+
+class Task extends CI_Controller {
+	
+	/**
+	 * 
+	 */
+	function index() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	function add() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	function edit() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	function delete() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	function checktask() {
+		
+	}
+	
+}
---
+++ application/controllers/user.php
@@ -0,0 +1,30 @@
+<?php
+
+class User extends CI_Controller {
+	
+	/**
+	 * This is the user's profile page.
+	 * Visible only if the user is logged in.
+	 */
+	function index() {
+		
+	}
+	
+	/**
+	 * This is the page where the user can edit his profile 
+	 * and his details.
+	 */
+	function editprofile() {
+		
+	}
+	
+	/**
+	 * This is a special method which will be available only to admins
+	 * but the admin should not be able to see any other data except for the user profile
+	 * and basic data...
+	 */
+	function deleteuser() {
+		
+	}
+	
+}
